e2d2f4f1330000b840d7be43d5421cd6ee7b7d55
Andrey Kudlay <matroskin8@i.ua>
2022-06-20 23:01:59 +0300

    Remove variable . Add constanses

--- incs/db.php
+++ incs/db.php
@@ -1,16 +1,9 @@
 <?php
 
-$db = [
-    'host' => DB_HOST,
-    'user' => DB_USER,
-    'pass' => DB_PASS,
-    'db' => DB_NAME,
-];
-
-$dsn = "mysql:host={$db['host']};dbname={$db['db']};charset=utf8";
+$dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8";
 $opt = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
 
-$pdo = new PDO($dsn, $db['user'], $db['pass'], $opt);
+$pdo = new PDO($dsn, DB_USER, DB_PASS, $opt);
